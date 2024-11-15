abb9789444ae00079e3c56d67cc987d67e97d26c
Mark Buckle <markbuckle92@gmail.com>
2024-11-15 20:18:39 -0330

    movie code

---
+++ src/main/java/dev/mark/movieAPI/ReviewService.java
@@ -0,0 +1,27 @@
+package dev.mark.movieAPI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepository; // insert reviewRepository into the database
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+    // look for the movie with the given imdbId then create review and associate it with imdbId
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+    }
+}
